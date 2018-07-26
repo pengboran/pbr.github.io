f499512ed0cbe7f0f85df276f8a5d10c1d384d75
pbr <237523733@qq.com>
2018-07-26 14:51:37 +0800

    文件替换
    文件替换

--- js/data.js
+++ js/data.js
@@ -7,13 +7,13 @@ var data = [
         },
          {
         	title:"乡愁",
-        	imgUrl:"https://mmbiz.qpic.cn/mmbiz_jpg/kRdlKAh3af6lgs0IwVQLgtZq2xzzgB26gsajt1YM3Cia5kPhnLzxtPPafxcmpaiaSr0udUeO9QjXicFa6kOHsUibRw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1",
+        	imgUrl:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532597631824&di=b99c3977e8558d61d71ddf6e110f33f6&imgtype=0&src=http%3A%2F%2Fs7.sinaimg.cn%2Fmw690%2F005ROrA1zy79PEQ6s5M26%26amp%3B690",
         	dec:"小时候，乡愁是一枚小小的邮票， 我在这头， 母亲在那头。 ",
         	toLink:"https://mp.weixin.qq.com/s?__biz=MzU5MDA2MzE0NA==&mid=2247483674&idx=1&sn=d4e4a6eabfda9576bdbe64632e8cdf79&chksm=fdc2b25dcab53b4b2f911def457b20bc85d5d6608c1fca1a60b183c4e7e9266db2dfd37d0ed5#rd"
         },
         {
         	title:"红颜",
-        	imgUrl:"https://mmbiz.qpic.cn/mmbiz_jpg/kRdlKAh3af6Dcic7ibBZbzribIZkFRx8ql5x071IZGMF08ERSywicic6G0sBrFTUmVPmynUF1kgL3ClJNeeOrlybczA/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1",
+        	imgUrl:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532597678633&di=58b3853e53993495b405ace843e696fb&imgtype=0&src=http%3A%2F%2Fs9.sinaimg.cn%2Fmw690%2F001xpTX2ty71U7WtrTi28%26690",
         	dec:"剑煮酒无味 饮一杯为谁，你为我送别 你为我送别胭脂香味 能爱不能给，天有多长 地有多远",
         	toLink:"https://mp.weixin.qq.com/s?__biz=MzU5MDA2MzE0NA==&tempkey=OTY3X0IvZ05sKzk5aXQrUDlTVWIzc0g0cU5ZekZUemxYVk9LcEJWeFE5SVNPZjN0V0FXaXN3MV9CcXFDaUNmYUxMS19GTHdjQ2ZLWGMwbjBOWlI4ZkJFcEN1MG51VElRWk0xN0pKNVlNVk1uTWYwSEFQNkRDdHJ0emhiZi1kYjBtVUljYzJtcVFaazNEUmkxQVNMaWFycEs5S2NVcXhLY3FFMEh4Y0pJTHd%2Bfg%3D%3D&chksm=7dc2b25a4ab53b4c4bf4e9867ec153644c0cd22d01b100a857b52753d1b5e89e73249627afa7#rd"
         }
