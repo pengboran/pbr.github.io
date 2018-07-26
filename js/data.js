75a7a590623b5e3020836b718c86f90e88eee6a0
pbr <237523733@qq.com>
2018-07-26 14:42:51 +0800

    数据修改
    数据修改

---
+++ js/data.js
@@ -0,0 +1,23 @@
+var data = [
+        {
+        	title:"前方的路",
+        	imgUrl:"https://pengboran.github.io/img/shihua1.png",
+        	dec:"慢慢的习惯了，你不在的时候，慢慢的感觉只剩一丝孤独。慢慢的我停留在原地，等待着你，再也没有见到远方的你。我没有檫去记忆的橡皮...",
+        	toLink:"https://mp.weixin.qq.com/s?__biz=MzU5MDA2MzE0NA==&mid=2247483654&idx=1&sn=236748711d305d51ebcc2fb09426bb6a&chksm=fdc2b241cab53b576d4e9c14eb04d317589ee21d1d0f1150abb17e1dec9ca500dbb6c07be9cc#rd"
+        },
+         {
+        	title:"乡愁",
+        	imgUrl:"https://mmbiz.qpic.cn/mmbiz_jpg/kRdlKAh3af6lgs0IwVQLgtZq2xzzgB26gsajt1YM3Cia5kPhnLzxtPPafxcmpaiaSr0udUeO9QjXicFa6kOHsUibRw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1",
+        	dec:"小时候，乡愁是一枚小小的邮票， 我在这头， 母亲在那头。 ",
+        	toLink:"https://mp.weixin.qq.com/s?__biz=MzU5MDA2MzE0NA==&mid=2247483674&idx=1&sn=d4e4a6eabfda9576bdbe64632e8cdf79&chksm=fdc2b25dcab53b4b2f911def457b20bc85d5d6608c1fca1a60b183c4e7e9266db2dfd37d0ed5#rd"
+        },
+        {
+        	title:"红颜",
+        	imgUrl:"https://mmbiz.qpic.cn/mmbiz_jpg/kRdlKAh3af6Dcic7ibBZbzribIZkFRx8ql5x071IZGMF08ERSywicic6G0sBrFTUmVPmynUF1kgL3ClJNeeOrlybczA/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1",
+        	dec:"剑煮酒无味 饮一杯为谁，你为我送别 你为我送别胭脂香味 能爱不能给，天有多长 地有多远",
+        	toLink:"https://mp.weixin.qq.com/s?__biz=MzU5MDA2MzE0NA==&tempkey=OTY3X0IvZ05sKzk5aXQrUDlTVWIzc0g0cU5ZekZUemxYVk9LcEJWeFE5SVNPZjN0V0FXaXN3MV9CcXFDaUNmYUxMS19GTHdjQ2ZLWGMwbjBOWlI4ZkJFcEN1MG51VElRWk0xN0pKNVlNVk1uTWYwSEFQNkRDdHJ0emhiZi1kYjBtVUljYzJtcVFaazNEUmkxQVNMaWFycEs5S2NVcXhLY3FFMEh4Y0pJTHd%2Bfg%3D%3D&chksm=7dc2b25a4ab53b4c4bf4e9867ec153644c0cd22d01b100a857b52753d1b5e89e73249627afa7#rd"
+        }
+
+
+
+]
